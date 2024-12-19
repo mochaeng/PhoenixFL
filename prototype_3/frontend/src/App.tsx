f5c00916fe7810d3f51b54d4b422ad2af026cb65
Luiz Campos <campos.antoonio@gmail.com>
2024-12-19 18:42:20 -0300

    feat: getting packets from websockets

--- prototype_3/frontend/src/App.tsx
+++ prototype_3/frontend/src/App.tsx
@@ -1,9 +1,32 @@
+import useWebSocket, { ReadyState } from "react-use-websocket";
 import "./App.css";
 
+const WS_URL = "ws://localhost:8080/live-classifications";
+
 function App() {
+  const { lastMessage, readyState } = useWebSocket(WS_URL, {
+    shouldReconnect: (closedEvent) => {
+      const { code } = closedEvent;
+      console.log("websocket connection was closed with code: ", code);
+      return true;
+    },
+    reconnectAttempts: 10,
+    reconnectInterval: (attemptNumber) =>
+      Math.min(Math.pow(2, attemptNumber) * 1000, 10000),
+  });
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: "Connecting",
+    [ReadyState.OPEN]: "Open",
+    [ReadyState.CLOSING]: "Closing",
+    [ReadyState.CLOSED]: "Closed",
+    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+  }[readyState];
+
   return (
     <>
-      <div></div>
+      <div>{lastMessage?.data}</div>
+      <div>{connectionStatus}</div>
     </>
   );
 }
