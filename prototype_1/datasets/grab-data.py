0dbc2ebde4d73fe0bd9f699ba226115751b19988
Luiz Campos <campos.antoonio@gmail.com>
2025-01-02 17:58:27 -0300

    feat(prototype_01): removing unnecessary columns

--- prototype_1/datasets/grab-data.py
+++ prototype_1/datasets/grab-data.py
@@ -4,26 +4,39 @@ import joblib
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
-from pre_process.pre_process import COLUMNS_TO_REMOVE, get_df
+from pre_process.pre_process import get_df
 
 DATASETS_PATH = "datasets/pre-processed/phoenix"
 PATH_TO_SAVE = "datasets/data-for-prototype-02"
 
+COLUMNS_TO_REMOVE = [
+    "IPV4_SRC_ADDR",
+    "IPV4_DST_ADDR",
+    "L4_SRC_PORT",
+    "L4_DST_PORT",
+]
+
 if __name__ == "__main__":
     ton_dataset = os.path.join(DATASETS_PATH, "NF-TON-IOT-V2.parquet")
-    df = get_df(ton_dataset, is_drop=False).sample(frac=1)
+    df = (
+        get_df(ton_dataset, is_drop=False)
+        .sample(frac=1)
+        .drop(columns=["Label", "Attack"])
+    )
+    print(df)
+
     df_file_path = os.path.join(PATH_TO_SAVE, "lines.csv")
     lines = df.iloc[:10000]
     lines.to_csv(df_file_path, index=False)
 
-    columns = [*COLUMNS_TO_REMOVE, "Label"]
+    columns = [*COLUMNS_TO_REMOVE]
     df_for_scale = df.drop(columns=columns)
     scaler = MinMaxScaler()
     scaler = scaler.fit(df_for_scale.values, df_for_scale.columns)
     scaler_file_path = os.path.join(PATH_TO_SAVE, "scaler.pkl")
     joblib.dump(scaler, scaler_file_path)
 
-    print(lines["Attack"].value_counts())
+    # print(lines["Attack"].value_counts())
 
     # print(type(df.iloc[0]))  # pandas series
     # print(len(df.iloc[0]))
